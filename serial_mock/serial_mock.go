d0d6339142f86cea8a6beb6e5fbf1a100d9e073a
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-05-24 14:16:23 +0200

    WIP

--- serial_mock/serial_mock.go
+++ serial_mock/serial_mock.go
@@ -12,7 +12,7 @@ func Receive_loop() {
 
 	// every time our ticker ticks
 	for t := range ticker.C {
-		output := fmt.Sprintf(t.Format("15:04:05"))
+		output := fmt.Sprintf(t.Format("15:04:05 "))
 		fmt.Printf("Serial mock: %s\n", output)
 
 		//Send output to the websocket go-routine
