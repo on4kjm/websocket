03ae071ac15a15d058e858c7c580964591d14d2e
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-05-24 21:12:04 +0200

    Now we are talking (scroll solved)

--- serial_mock/serial_mock.go
+++ serial_mock/serial_mock.go
@@ -10,9 +10,15 @@ var MessageBuffer = make(chan string, 10)
 func Receive_loop() {
 	ticker := time.NewTicker(5 * time.Second)
 
+	i := 0
 	// every time our ticker ticks
 	for t := range ticker.C {
 		output := fmt.Sprintf(t.Format("15:04:05 "))
+		i++
+		if(i == 4) {
+			output = output + "\n"
+			i = 0
+		}
 		fmt.Printf("Serial mock: %s\n", output)
 
 		//Send output to the websocket go-routine
